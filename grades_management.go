6e1c44f805a3b24ea566936d2e16aa7323c4cc0e
Darwin Arevalo <dalexis.da@gmail.com>
2021-03-23 21:37:16 -0600

    Se pasan las celdas y el nombre del archivo de notas por flags

--- grades_management.go
+++ grades_management.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
+	"path/filepath"
+	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 func getNameWithExt(fileName, ext string) string {
-	return fileName + "." + ext
+	return fileName + ext
 }
 
 func extractGrades(xlsx *excelize.File, carneCell, gradeCell string) {
@@ -22,16 +25,31 @@ func extractGrades(xlsx *excelize.File, carneCell, gradeCell string) {
 	}
 }
 
+var gradeCell string
+var carneCell string
+var fileName string
+
 func main() {
-	fileName := "Notas"
-	ext := "xlsx"
+
+	flag.StringVar(&gradeCell, "nota", "", "La celda del excel donde esta la nota del alumno ej: A10")
+	flag.StringVar(&carneCell, "carnet", "", "La celda del excel donde esta el carnet del alumno ej: D100")
+	flag.StringVar(&fileName, "archivo", "", "La ruta del archivo xlsx")
+	flag.Parse()
+
+	if gradeCell == "" || carneCell == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	ext := filepath.Ext(fileName)
+	fileName = strings.Replace(fileName, ext, "", 1)
 
 	original := getNameWithExt(fileName, ext)
 
 	xlsx, err := excelize.OpenFile(original)
 	if err != nil {
-		log.Fatal(errors.New("no se pudo abrir el archivo" + original))
+		log.Fatal(errors.New("no se pudo abrir el archivo " + original))
 	}
 
-	extractGrades(xlsx, "D3", "D103")
+	extractGrades(xlsx, carneCell, gradeCell)
 }
